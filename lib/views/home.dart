724aee231626e73a76466e0ecbd496dc57dbc6a2
Deepak <ideepakrabindra@gmail.com>
2024-05-20 10:35:17 +0530

    feat: Add initial project files and structure

---
+++ lib/views/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello"),
+      ),
+      body: Center(
+        child: Text("From Home"),
+      ),
+    );
+  }
+}
