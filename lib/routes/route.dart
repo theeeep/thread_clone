c85b51553dbad74984bb67030c8cd7a1610b6115
Deepak <ideepakrabindra@gmail.com>
2024-05-20 12:30:04 +0530

    refactor: move auth and home pages to features folder

--- lib/routes/route.dart
+++ lib/routes/route.dart
@@ -1,8 +1,8 @@
 import 'package:get/route_manager.dart';
-import 'package:thread_clone/pages/auth/login_page.dart';
-import 'package:thread_clone/pages/auth/signup_page.dart';
+import 'package:thread_clone/features/auth/pages/login_page.dart';
+import 'package:thread_clone/features/auth/pages/signup_page.dart';
 import 'package:thread_clone/routes/route_names.dart';
-import 'package:thread_clone/pages/home_page.dart';
+import 'package:thread_clone/features/home_page.dart';
 
 class Routes {
   static final pages = [
